db1a4afc941773f4e6aa944cf76615b42d7aa4db
nimesh vaghasiya <nimesh.vaghasiya95@live.com>
2023-04-06 12:16:03 +0530

    latest template fetch changes

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,5 +1,6 @@
+/** @type {import("@babel/core").ConfigFunction} */
 module.exports = function (api) {
-  api.cache(true);
+  api.cache.forever();
 
   // Make Expo Router run from `src/app` instead of `app`.
   // Path is relative to `/node_modules/expo-router`
